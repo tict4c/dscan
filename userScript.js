58cbe969622c00ad87f8b585d96401f303cf94bf
tictac <tict4c1@gmx.de>
2024-11-04 20:54:25 +0100

    Create userScript.js

---
+++ userScript.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Faction Grouper Remote Source
+// @namespace    http://tampermonkey.net/
+// @version      1.2
+// @description  Group faction IDs and display them at the top of the page, checking every 5 seconds for the first 30 seconds
+// @author       Servanda
+// @match        https://scan.eveonline.it/*
+// @require      https://raw.githubusercontent.com/tict4c/dscan/main/dscanMonkey.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your Tampermonkey script logic here
+    // The script from the @require directive will be loaded and executed before this point
+})();
